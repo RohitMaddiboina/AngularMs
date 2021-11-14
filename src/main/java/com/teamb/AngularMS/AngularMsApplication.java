6cb7d9daf4807afa28217ea75db94e5cabd26eeb
tirumalasettyvk98 <tirumalasettyvk98@gmail.com>
2021-11-14 18:28:19 +0530

    angular ms Eureka added

---
+++ src/main/java/com/teamb/AngularMS/AngularMsApplication.java
@@ -0,0 +1,13 @@
+package com.teamb.AngularMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamb/AngularMS/AngularMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamb.AngularMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
